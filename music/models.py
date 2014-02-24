da337c9686636363b3c22acddc179b45529074d3
Cevaris <cevaris@gmail.com>
2014-02-23 17:39:20 -0700

    created user; got rendering down

--- music/models.py
+++ music/models.py
@@ -2,16 +2,45 @@ import datetime
 from flask import url_for
 from music import db
 
+class Album(db.Document):
+    slug = db.StringField(max_length=255, required=False)
+    name = db.StringField(max_length=255, required=True)
+    publisher = db.StringField(max_length=255, required=True)
+    genre = db.StringField(max_length=255, required=True)
+    media_type = db.StringField(max_length=255, required=True)
+    name = db.StringField(max_length=255, required=True)
+    
+    def get_absolute_url(self):
+        return url_for('album', kwargs={"slug": self.slug})
+
+    def __unicode__(self):
+        return self.name
+
+
 
 class Artist(db.Document):
 
-    slug = db.StringField(max_length=255, required=False)
     name = db.StringField(max_length=255, required=True)
-    created_at = db.DateTimeField(default=datetime.datetime.now, required=True)
+    slug = db.StringField(max_length=255, required=False)
+    albums = db.ListField(db.ReferenceField('Album'))
 
 
     def get_absolute_url(self):
-        return url_for('post', kwargs={"slug": self.slug})
+        return url_for('artist', kwargs={"slug": self.slug})
 
     def __unicode__(self):
-        return self.title
+        return self.name
+
+
+class User(db.Document):
+
+    name = db.StringField(max_length=255, required=True)
+    slug = db.StringField(max_length=255, required=False)
+    email = db.StringField(max_length=255, required=False)
+
+    def __unicode__(self):
+        return "%s, Email:%s" % (self.name, self.email)
+
+
+
+
--- music/views.py
+++ music/views.py
@@ -3,7 +3,7 @@ from flask import Blueprint, request, redirect, render_template, url_for
 from flask.views import MethodView
 
 from flask.ext.mongoengine.wtf import model_form
-from music.models import Artist
+from music.models import *
 
 music = Blueprint('music', __name__, template_folder='templates')
 
@@ -28,6 +28,12 @@ class Home(MethodView):
 
   def get(self):
     return render_template('music/home.html')
+
+class Users(MethodView):  
+
+  def get(self):
+    users = User.objects.all()
+    return render_template('music/users.html',users=users)
 
 # class CreateArticle(MethodView):
 
@@ -56,6 +62,7 @@ class Home(MethodView):
 
 
 music.add_url_rule('/', view_func=Home.as_view('home'))
+music.add_url_rule('/users', view_func=Users.as_view('users'))
 # articles.add_url_rule('/create', view_func=CreateArticle.as_view('create'))
 # articles.add_url_rule('/view/<slug>/', view_func=ShowArticle.as_view('show'))
 # articles.add_url_rule('/search', view_func=Search.as_view('search'))
